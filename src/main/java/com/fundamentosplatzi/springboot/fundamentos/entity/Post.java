4864286e00183d18955de8e4267d946da1ea2847
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-16 10:11:41 +0100

    tut17

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/entity/Post.java
@@ -0,0 +1,30 @@
+package com.fundamentosplatzi.springboot.fundamentos.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "post")
+@Getter
+@Setter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_post", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @ManyToOne
+    private User user;
+
+    public Post() {
+    }
+
+    public Post(String description, User user) {
+        this.description = description;
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/entity/User.java
@@ -0,0 +1,43 @@
+package com.fundamentosplatzi.springboot.fundamentos.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user", nullable = false, unique = true)
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(length = 50)
+    private String email;
+
+    private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Post> posts = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String name, String email, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+}
