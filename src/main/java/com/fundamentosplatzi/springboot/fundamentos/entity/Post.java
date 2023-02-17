530423e21ab12a3513db8c2b58bb78551e60624c
Julia Tortosa <julia.tortosa@ad.upcnet.es>
2023-02-17 09:08:24 +0100

    tut29

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/entity/Post.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/entity/Post.java
@@ -1,5 +1,6 @@
 package com.fundamentosplatzi.springboot.fundamentos.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +21,7 @@ public class Post {
     private String description;
 
     @ManyToOne
+    @JsonBackReference
     private User user;
 
     public Post() {
